491c485ad5600d126a07d5e0670f368cbcdc533b
Amin <asaa47721@gmail.com>
2024-10-17 22:39:14 +0330

    Fix shop categories api and add product categories api

--- app/api/v1/endpoints/category.py
+++ app/api/v1/endpoints/category.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Request
 
-from app.models.category import Category
+from app.models.category import ProductCategory, ShopCategory
 from app.schemas.category import CategoryRead
 from app.schemas.token import TokenData
 from app.utils import get_current_user
@@ -8,12 +8,23 @@ from app.utils import get_current_user
 router = APIRouter()
 
 
-@router.get("")
-async def list_category_api(
+@router.get("/shop")
+async def list_shop_category_api(
     request: Request,  # noqa: ARG001
     user: TokenData = Depends(get_current_user),  # noqa: ARG001
 ) -> list[CategoryRead]:
-    """List of categories"""
+    """List of shop categories"""
 
-    categories = await Category.filter(is_active=True)
+    categories = await ShopCategory.filter(is_active=True)
+    return categories
+
+
+@router.get("/product")
+async def list_product_category_api(
+    request: Request,  # noqa: ARG001
+    user: TokenData = Depends(get_current_user),  # noqa: ARG001
+) -> list[CategoryRead]:
+    """List of product categories"""
+
+    categories = await ProductCategory.filter(is_active=True)
     return categories
