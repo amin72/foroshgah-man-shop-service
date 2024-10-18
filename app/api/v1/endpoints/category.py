288ee17501f98110fa63bb8e85ed8d2854dc2c91
Amin <asaa47721@gmail.com>
2024-10-19 02:03:58 +0330

    separate schemas for shop and product categories

--- app/api/v1/endpoints/category.py
+++ app/api/v1/endpoints/category.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 
 from app.models.category import ProductCategory, ShopCategory
-from app.schemas.category import CategoryRead
+from app.schemas.category import ProductCategoryRead, ShopCategoryRead
 from app.schemas.token import TokenData
 from app.utils import get_current_user
 
@@ -12,7 +12,7 @@ router = APIRouter()
 async def list_shop_category_api(
     request: Request,  # noqa: ARG001
     user: TokenData = Depends(get_current_user),  # noqa: ARG001
-) -> list[CategoryRead]:
+) -> list[ShopCategoryRead]:
     """List of shop categories"""
 
     categories = await ShopCategory.filter(is_active=True)
@@ -23,7 +23,7 @@ async def list_shop_category_api(
 async def list_product_category_api(
     request: Request,  # noqa: ARG001
     user: TokenData = Depends(get_current_user),  # noqa: ARG001
-) -> list[CategoryRead]:
+) -> list[ProductCategoryRead]:
     """List of product categories"""
 
     categories = await ProductCategory.filter(is_active=True)
--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,19 +1,34 @@
 from pydantic import BaseModel
 
 
+def category_example(title: str = "Category name", include_shop_category: bool = False):
+    """Utility function to generate example configurations"""
+
+    example = {
+        "id": "01F8MECHZX3TBDSZ7XRADM79XV",
+        "title": title,
+        "image": "URL",
+    }
+
+    if include_shop_category:
+        example["shop_category_id"] = "02F8MECHZX3TBDSZ7XRADM70XV"
+
+    return {"json_schema_extra": {"examples": [example]}}
+
+
 class CategoryRead(BaseModel):
     id: str
     title: str
     image: str
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "id": "01F8MECHZX3TBDSZ7XRADM79XV",
-                    "name": "Category name",
-                    "image": "URL",
-                }
-            ]
-        }
-    }
+    model_config = category_example()
+
+
+class ShopCategoryRead(CategoryRead):
+    pass
+
+
+class ProductCategoryRead(CategoryRead):
+    shop_category_id: str
+
+    model_config = category_example(include_shop_category=True)
