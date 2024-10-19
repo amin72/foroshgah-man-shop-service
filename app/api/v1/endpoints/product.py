5c633b7ea4f52d3fb20f96b8fb22557a2c3eda0c
Amin <asaa47721@gmail.com>
2024-10-19 04:40:44 +0330

    Add update product api

--- app/api/v1/endpoints/product.py
+++ app/api/v1/endpoints/product.py
@@ -5,7 +5,7 @@ from tortoise.transactions import in_transaction
 from app.models.category import ProductCategory
 from app.models.product import Product
 from app.models.shop import Shop
-from app.schemas.product import ProductCreate
+from app.schemas.product import ProductCreate, ProductUpdate
 from app.schemas.token import TokenData
 from app.utils import get_current_user
 
@@ -40,3 +40,54 @@ async def add_product(
         )
 
     return new_product
+
+
+@router.patch("/{id}", response_model=ProductUpdate)
+async def update_product_api(
+    id: str,
+    product: ProductUpdate,
+    user: TokenData = Depends(get_current_user),
+):
+    """
+    Update product API
+    """
+
+    shop = await Shop.get_or_none(owner_id=user.user_id)
+
+    if shop is None:
+        raise HTTPException(status_code=404, detail="Shop not found")
+
+    product_obj = await Product.get_or_none(id=id)
+
+    if product_obj is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    if product_obj.shop_id != shop.id:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    product_data: dict = product.model_dump()
+
+    if product.is_active is None:
+        del product_data["is_active"]
+
+    # Check category
+    product_category = await ProductCategory.get(id=product.category_id)
+
+    product_shop_category_id = (await product_category.shop_category).id
+    shop_category_id = (await shop.category).id
+
+    if product_shop_category_id != shop_category_id:
+        raise HTTPException(status_code=400, detail="Wrong category")
+
+    # Validate the category if provided
+    if product.category_id:
+        category = await ProductCategory.get_or_none(id=product.category_id)
+
+        if category is None:
+            raise HTTPException(status_code=404, detail="Category not found")
+
+        product_data["category"] = category
+
+    await product_obj.update_from_dict(product_data).save()
+
+    return product_obj
--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
 
 
+product_model_config = {
+    "json_schema_extra": {
+        "examples": [
+            {
+                "name": "Shop name",
+                "description": "Shop description",
+                "price": 100000,
+                "is_active": True,
+                "category_id": "01F8MECHZX3TBDSZ7XRADM79XV",
+            }
+        ]
+    }
+}
+
+
 class ProductCreate(BaseModel):
     name: str
     description: str | None
@@ -8,16 +23,14 @@ class ProductCreate(BaseModel):
     is_active: bool = True
     category_id: str
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Shop name",
-                    "description": "Shop description",
-                    "price": 100000,
-                    "is_active": True,
-                    "category_id": "01F8MECHZX3TBDSZ7XRADM79XV",
-                }
-            ]
-        }
-    }
+    model_config = product_model_config
+
+
+class ProductUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: int | None = None
+    is_active: bool | None = None
+    category_id: str | None = None
+
+    model_config = product_model_config
